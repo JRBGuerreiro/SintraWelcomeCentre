11ce58441e96b0f348f4001718051d5fbe37e5cd
Jorge Guerreiro <crcguerreiro@gmail.com>
2021-09-05 15:25:12 +0100

    novo backgeound

--- sintrawelcomecenntre/src/components/HeroSection.js
+++ sintrawelcomecenntre/src/components/HeroSection.js
@@ -1,12 +1,13 @@
 import React from "react";
 import heroImage from "../images/hero.jpg"
 import logo from "../images/logo.png"
+import bike from "../images/bike.svg"
 
 const HeroSection = () => {
     return (
         <section className="hero_section">
-            {/* <img className="hero_img" src={heroImage} alt="Lake"/> */}
-                <img src={logo} alt="logo"></img>
+            <img className="hero_img" src={heroImage} alt="Lake"/>
+                <img className="logo" src={logo} alt="logo"></img>
                 <h1>Coming Soon</h1>
             {/* <nav className = "hero_nav">
                 <ul>
