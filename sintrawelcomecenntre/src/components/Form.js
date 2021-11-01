2a31ccc8bb593f428422a2a8a290dde05656fbec
Jorge Guerreiro <crcguerreiro@gmail.com>
2021-11-01 17:06:28 +0100

    apaga coisas amigo

--- sintrawelcomecenntre/src/components/Form.js
+++ sintrawelcomecenntre/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useLayoutEffect, useEffect} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 import styled from "styled-components";
 import {MdContactMail} from "react-icons/md"
 import useForm from "./useForm"
