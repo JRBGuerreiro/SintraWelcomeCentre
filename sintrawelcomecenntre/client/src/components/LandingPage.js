7a72e51b8a3e3f9460433b739885d3ca3222daf7
Jorge Guerreiro <jguerreiro@adaptavist.com>
2022-06-10 08:16:11 +0200

    refactor

---
+++ sintrawelcomecenntre/client/src/components/LandingPage.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from "react";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import Products from "./Products";
+import Form from "./Form";
+import Gallery from "./Gallery";
+import Maps from "./Maps";
+
+const LandingPage = () => {
+
+    const [lang, setLang] = useState("en")
+    const changeLanguage = (language) => setLang(language)
+
+    useEffect(async() => {
+      debugger
+      const resp = await fetch("https://server.sintrawelcomecentre.com/api")
+      resp.json().then(d => console.log(d))
+    }, [])
+
+    return (
+            <>
+                <div className="App">
+                  <HeroSection
+                    changeLang = {changeLanguage}
+                    language = {lang}
+                  />
+                    <Products
+                    language = {lang}
+                    />
+                    <Maps 
+                      language= {lang}
+                    />
+                    <Gallery
+                       language = { lang }
+                    />
+                  <Form
+                    language= {lang}
+                  />
+                  <Footer
+                    language = { lang }
+                  />
+                </div>
+                {/* <EventModal
+                  language = { lang }
+                /> */}
+            </>
+            
+            )
+}
+
+export default LandingPage;
