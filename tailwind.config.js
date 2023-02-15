ec76cd5127016e94f2e266910972f695db8b9f32
rubenbrandao <rubenavbrandao@gmail.com>
2023-02-15 23:47:24 +0000

    chore: updated config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,32 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {
-      screens: {
-        tablet: '640px',
-        // => @media (min-width: 640px) { ... }
-        laptop: '1024px',
-        // => @media (min-width: 1024px) { ... }
-        desktop: '1280px'
-        // => @media (min-width: 1280px) { ... }
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif']
-      },
-      colors: {
-        green: '#3dc050',
-        'light-gray': '#dce4e4',
-        'dark-gray': '#333333',
-        black: '#0e0e0e'
-      }
-    }
+    screens: {
+      tablet: '640px',
+      // => @media (min-width: 640px) { ... }
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+      desktop: '1280px'
+      // => @media (min-width: 1280px) { ... }
+    },
+    fontFamily: {
+      inter: ['Inter', 'sans-serif'],
+      raleway: ['Raleway', 'sans-serif']
+    },
+    colors: {
+      black: '#000',
+      white: '#fff',
+      night: '#0e0e0e',
+      'light-green': '#3dc050',
+      'forest-green': '#2e933c',
+      'light-gray': '#dce4e4',
+      'dark-gray': '#333333',
+      red: '#ff0000' //used for debugging
+    },
+    backgroundImage: {
+      hero: "url('../public/assets/images/hero-bg.jpg')"
+    },
+    extend: {}
   },
   variants: {
     typography: ['dark']
