037565e16e8349407061306f0168fc12637efe4e
rubenbrandao <rubenavbrandao@gmail.com>
2023-03-02 22:41:38 +0000

    feat: product packages json file first iteration and new svg icons

---
+++ components/svg/Clock.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from 'react';
+
+const Clock = (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={2.5}
+    stroke="currentColor"
+    {...props}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+);
+
+export default Clock;
---
+++ utils/product-packages.ts
@@ -0,0 +1,47 @@
+type ProductPackage = {
+  id: number;
+  type: 'product';
+  name: string;
+  image: string;
+  description: string;
+  location: string;
+  price: {
+    type: 'individual' | 'group';
+    rate: 'hourly' | 'daily' | 'weekly' | 'monthly';
+    value: number;
+    currency: string;
+  };
+  duration: {
+    type: 'hour' | 'day' | 'week' | 'month';
+    value: number;
+  };
+  limit: {
+    type: 'person' | 'group';
+    value: number;
+  };
+};
+
+export default [
+  {
+    id: 1,
+    type: 'product',
+    name: 'Product 1',
+    image: 'https://picsum.photos/200/300',
+    description: 'Product 1 description',
+    location: 'Singapore',
+    price: {
+      type: 'per person',
+      rate: 'hourly',
+      value: 100,
+      currency: 'EUR'
+    },
+    duration: {
+      type: 'day',
+      value: 1
+    },
+    limit: {
+      type: 'person',
+      value: 10
+    }
+  }
+];
